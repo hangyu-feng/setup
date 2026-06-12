a5804e28f22ffbca82d107430765959429b2ccbd
Feng,Hangyu <fhy@uw.edu>
2026-06-11 20:51:39 -0700

    Fix ripgrep config path through fish symlink

--- configs/fish/conf.d/path.fish
+++ configs/fish/conf.d/path.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     export PATH="$(uv tool dir --bin):$PATH"
-    set -l ripgrep_config_dir (dirname (status --current-filename))
+    set -l ripgrep_config_dir (dirname (path resolve (status --current-filename)))
     set -gx RIPGREP_CONFIG_PATH (path normalize "$ripgrep_config_dir/../../ripgreprc")
 end
